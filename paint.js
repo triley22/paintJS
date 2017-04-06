d39d24548e4ff14caf972599103a9d895b00a3cf
triley22 <triley09@gmail.com>
2017-04-06 09:46:42 -0700

    tool selection

--- paint.js
+++ paint.js
@@ -41,3 +41,98 @@ function createPaint(parent) {
 	var panel = elt("div", {class: "picturepanel"}, canvas);
 	parent.appendChild(elt("div", null, panel, toolbar));
 }
+
+//Tool Selection
+
+//as with controls, object to collect various tools and add more later
+
+var tools = Object.create(null);
+
+controls.tool = function(cx) {
+	var select = elt("select");
+	for (var name in tools)
+		select.appendChild(elt("option", null, name));
+
+	cx.canvas.addEventListener("mousedown", function(event) {
+		if (event.which == 1) {
+			tools[select.value](event, cx);
+			event.preventDefault();
+		}
+	});
+
+	return elt("span", null, "Tool: ", select);
+};
+
+//clientX and clientY properties on mouse events are also relative to top-left corner
+
+function relativePos(event, element) {
+	var rect = element.getBoundingClientRect();
+	return {x: Math.floor(event.clientX - rect.left),
+					y: Math.floor(event.clientY - rect.top)};
+}
+
+//function takes two arguments
+//one calls for each mousemove event and other when released
+
+function trackDrag(onMove, onEnd) {
+	function end(event) {
+		removeEventListener("mousemove", onMove);
+		removeEventListener("mouseup", end);
+		if (onEnd)
+			oneEnd(event);
+	}
+	addEventListener("mousemove", onMove);
+	addEventListener("mouseup", end);
+}
+
+
+//line tool uses two helpers to do the actual drawing
+//"mousemove" line segment is drawn between the mouse's old and new position
+
+tools.Line = function(event, cx, onEnd) {		//lineCap property to round instead of square
+	cx.lineCap = "round";											//trick to make sure separate lines look like a single coherent line
+
+	var pos = relativePos(event, cx.canvas);
+	trackDrag(function(event) {
+		cx.beginPath();
+		cx.moveTo(pos.x, pos.y);
+		pos = relativePos(event, cx.canvas);
+		cx.lineTo(pos.x, pos.y);
+		cx.stroke();
+	}, onEnd);
+};
+
+//globalCompositeOperation property influences drawing operations on a canvas change
+//change the color of the pixels they touch
+
+tools.Erase = function(event, cx) {
+	cx.globalCompositeOperation = "destination-out";
+	tools.Line(event, cx, function() {
+		cx.globalCompositeOperation = "source-over";
+	});
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
